e9dd16e75405aff2045164931a90fa3e14ba0bbd
Javier <javier.torres@edunext.co>
2022-07-14 23:43:48 -0700

    fix: this change must work only with CCXs.

--- lms/djangoapps/ccx/views.py
+++ lms/djangoapps/ccx/views.py
@@ -100,8 +100,10 @@ def coach_dashboard(view):
                 return view(request, course, ccx)
             else:
                 # If user has the staff role at CCX level, then he/she can view ccx coach dashboard for licensed ccxs.
-                ccx_id = CCXLocator.from_course_locator(course.id, six.text_type(ccx.id))
-                if is_staff_allowed_to_access_ccx_coach_tab(request.user, ccx_id):
+                if ccx and is_staff_allowed_to_access_ccx_coach_tab(
+                    request.user,
+                    CCXLocator.from_course_locator(course.id, six.text_type(ccx.id))
+                    ):
                     return view(request, course, ccx)
                 # if there is a ccx, we must validate that it is the ccx for this coach
                 role = CourseCcxCoachRole(course_key)
